7ba3b7760a3969816f17ff13e6c51efd406facd9
unknown <Administrator@ug-aup-n08.corp.ukrtelecom.loc>
2023-03-03 11:47:00 +0200

    added typing animation

--- public/userIsTyping.js
+++ public/userIsTyping.js
@@ -2,9 +2,10 @@ let timer;
 const userIsTyping = (userName) => {
     clearTimeout(timer);
     const isTypingTooltip = document.getElementById('typing-tooltip');
+    const whoIsTyping = document.getElementById('typing-name-printing');
     isTypingTooltip.style.display = 'flex';
-    isTypingTooltip.textContent = `${userName} is typing ...`;
-    timer = setTimeout(() => {isTypingTooltip.textContent = '', document.getElementById('typing-tooltip').style.display = 'none'}, 1000);
+    whoIsTyping.textContent = `${userName} is typing`;
+    timer = setTimeout(() => {whoIsTyping.textContent = '', document.getElementById('typing-tooltip').style.display = 'none'}, 1000);
 
 
 
